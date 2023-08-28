94d50b349b1bc9b998283ac4eaff9c3101f29067
lauax <lucas12alfredsson@gmail.com>
2023-08-28 19:30:57 +0200

    edit

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ export function HomePage() {
     
     return(
         <>
-        <div style={{height:'54vh', justifyContent:'center', textAlign:'center', display:'flex', marginTop:'20%'}}>
+        <div className='bodyhome' style={{height:'54vh', justifyContent:'center', textAlign:'center', display:'flex', marginTop:'20%'}}>
         <h1 style={{fontSize:'5rem'}}>Lucas-CV</h1>
         </div>
         
